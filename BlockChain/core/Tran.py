204e4afbd182a1ac4d143341e4c9edd0bde7be2b
mekyang <13792042833@163.com>
2021-12-20 22:24:12 +0800

    v1.0.8

--- BlockChain/core/Tran.py
+++ BlockChain/core/Tran.py
@@ -1,4 +1,12 @@
-class class1(object):
-    """description of class"""
+class Tran(object):
+    
+    def __init__(self):
+        pass
+
+    def construct_transaction(self):
+        pass
+
+    def send_transaction(self):
+        pass
 
 
